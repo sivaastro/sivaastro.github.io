b0de0eff75f70491c18ca62acd240edacfd69a71
Murali Murugan <murali732000@gmail.com>
2023-12-22 15:57:37 +0000

    commit the changes

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function showSidebar() {
+
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'block';
+    console.log("it's clicking");
+
+}
+
+function closeSidebar() {
+
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+    console.log("clicking closing");
+
+}
